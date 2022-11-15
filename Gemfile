c6f76969f5f12de1123c6a6222222cca06d30b57
fjys1203 <fjys.1203@gmail.com>
2022-11-16 01:00:39 +0900

    バリデーションの実装

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ group :production do
   gem 'pg'
 end
 gem 'devise'
+gem 'rspec-rails', '~> 4.0.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -11,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == "admin" && password == "2222"
     end
   end
 end
