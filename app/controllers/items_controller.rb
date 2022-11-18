f690597054147e2e4fca5d0b56a84fd3f679af87
fjys1203 <fjys.1203@gmail.com>
2022-11-18 15:51:57 +0900

    データの登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,26 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.order("created_at DESC")
   end
   
   def new
+    @item = Item.new
   end
   
   def create
+    @item = Item.new(item_prams)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+    private
+
+    def item_params
+      params.require(:item).permit(:item_name,:item_text,:category_id,:condition_id,:shopping_charge_id,:prefecture_id,:days_for_send_id,:price)
+    end
   end
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shopping_charge
+  belongs_to :prefecture
+  belongs_to :days_for_send
+
+
   belongs_to :user
   has_one :record
 
-  validation :item_name, presence: true
-  validation :item_text, presence: true
-  validation :category_id, presence: true
-  validation :condition_id, presence: true
-  validation :shopping_charge_id, presence: true
-  validation :prefecture_id, presence: true
-  validation :days_for_send_id, presence: true
-  validation :price, presence: true
-  validation :user, foreign_key: true
+  
+  validates :item_name, presence: true
+  validates :item_text, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shopping_charge_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :days_for_send_id, numericality: { other_than: 1 }
+  validates :price, presence: true
+  validates :user, foreign_key: true
 end
