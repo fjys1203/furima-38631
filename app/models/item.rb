7fbf7d2c900e51dd6b5c035e95c1eef7a3097b49
fjys1203 <fjys.1203@gmail.com>
2022-11-21 16:24:52 +0900

    商品一覧表示機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   
-  validates :user, presence: true
+  validates :user_id, presence: true
   validates :image, presence: true
   validates :item_name, presence: true
   validates :item_text, presence: true
