a25cab33a9f3080c84e25e45229775fcb667bfba
fjys1203 <fjys.1203@gmail.com>
2022-11-21 11:09:34 +0900

    ログインページの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
-    if user_signed_in? 
-    else
-     redirect_to user_session_path
-    end
+    @items = Item.includes(:user).order('created_at DESC')
   end
   
   def new
