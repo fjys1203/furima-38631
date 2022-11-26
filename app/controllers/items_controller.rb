49a6b795e3e1a8cdf6c077adbf1f5e2d0c929dd9
fjys1203 <fjys.1203@gmail.com>
2022-11-27 02:17:54 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :authenticate_user!, except: [:index]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -34,7 +34,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    if @item.order.present?
+    unless @item.order.nil?
       redirect_to root_path
     end
   end
